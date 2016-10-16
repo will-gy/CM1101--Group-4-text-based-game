06edd25f7a40f17e20ee2c5b2eabff1ab8d7d44f
Umberla <markuss.baumgarts@gmail.com>
2016-10-16 15:32:47 +0100

    Delete player.py

--- Markuss-group/player.py
+++
@@ -1,8 +0,0 @@
-from items import *
-from map import rooms
-
-inventory = [item_id, item_laptop, item_money]
-stats = {"health": 100, }
-
-# Start game at the reception
-current_room = rooms["Reception"]
