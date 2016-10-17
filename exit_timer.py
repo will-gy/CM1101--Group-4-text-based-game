e19272bc025ef20d8ccbdd533f7691f4138167ea
Will <william.yelverton@hotmail.com>
2016-10-17 17:22:27 +0100

    Add files via upload

---
+++ exit_timer.py
@@ -0,0 +1,13 @@
+from game_over import*
+if has_key == True:
+	print("THE WARDEN HAS SET OFF THE ALARM")
+	time.sleep(1)
+	print("YOU HAVE 3 MINUTES TO ESCAPE BEFORE YOU GET CAUGHT!")
+	time.sleep(60)
+	print("YOU HAVE TWO MINUTES LEFT TO ESCAPE")
+	time.sleep(60)
+	print("YOU HAVE ONE MINUTE LEFT TO ESCAPE")
+	time.sleep(10)
+	print("30 SECONDS, GET A MOVE ON")
+	time.sleep(1)
+game_over()
