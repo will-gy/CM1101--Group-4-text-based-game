dadd74b53eeafec38ef016f75812b171da5cd2f0
Will <william.yelverton@hotmail.com>
2016-10-17 16:44:17 +0100

    Update game_over.py

--- group project functions/game_over.py
+++ group project functions/game_over.py
@@ -1,22 +1,25 @@
 import time
 
-game_over_message =  \
-"""
-   _____          __  __ ______      ______      ________ _____  
-  / ____|   /\   |  \/  |  ____|    / __ \ \    / /  ____|  __ \ 
- | |  __   /  \  | \  / | |__      | |  | \ \  / /| |__  | |__) |
- | | |_ | / /\ \ | |\/| |  __|     | |  | |\ \/ / |  __| |  _  / 
- | |__| |/ ____ \| |  | | |____    | |__| | \  /  | |____| | \ \ 
-  \_____/_/    \_\_|  |_|______|    \____/   \/   |______|_|  \_\                                                               
-	"""
+def game_over(death):
 
-go_split = game_over_message.split('\n')
-for x in go_split:
-	print(x)
-	time.sleep(0.4)
 
-for x in range (0, 1):
-	time.sleep(0.2)
-	print()                                       
+ game_over_message =  \
+ """
+    _____          __  __ ______      ______      ________ _____
+   / ____|   /\   |  \/  |  ____|    / __ \ \    / /  ____|  __ \
+  | |  __   /  \  | \  / | |__      | |  | \ \  / /| |__  | |__) |
+  | | |_ | / /\ \ | |\/| |  __|     | |  | |\ \/ / |  __| |  _  /
+  | |__| |/ ____ \| |  | | |____    | |__| | \  /  | |____| | \ \
+   \_____/_/    \_\_|  |_|______|    \____/   \/   |______|_|  \_\
+ 	"""
 
-print("Dropping the soap is never a good idea in prison")
+ go_split = game_over_message.split('\n')
+ for x in go_split:
+ 	print(x)
+ 	time.sleep(0.4)
+
+ for x in range (0, 1):
+ 	time.sleep(0.2)
+ 	print()
+  print(str(death))
+#This should take a string as an argument and print it after the ASCII "game over" text.
