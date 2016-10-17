8831c0d6b100f56147798aff5adc92a9c22eda8f
Umberla <markuss.baumgarts@gmail.com>
2016-10-17 14:28:40 +0100

    Add files via upload

---
+++ Markuss - group/player.py
@@ -0,0 +1,18 @@
+from items import *
+from map import rooms
+from random import randint
+
+inventory = [item_soap]
+
+
+f = randint(0,2)
+if f ==1:
+    current_room = rooms["Cell A"]
+    start_room = current_room
+
+
+else:
+    current_room = rooms["Cell B"]
+    start_room = current_room
+
+
