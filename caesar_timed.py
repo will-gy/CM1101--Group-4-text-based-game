9962dba1c220f3ff05c9ea00b8bfa0a1d56dbacb
Will <william.yelverton@hotmail.com>
2016-10-19 18:42:21 +0100

    Add files via upload

---
+++ caesar_timed.py
@@ -0,0 +1,48 @@
+from threading import Thread
+import time
+from random import randint
+timer_time = ["3" , "2", "1"]
+game_win = False
+def exit_timer():
+	while game_win == False:
+		for x in timer_time:
+			print("You have " + str(x) + " minute(s) remaining!")
+			time.sleep(60)
+			if x == "1" and game_win==False:
+				print("game over")
+				return
+
+def caesar_encrypt():
+
+	x ="kirill is the best"
+	k = x.replace(" ", "")
+	pos = 0
+	text = list(k)
+	length = len(text)
+	pasw = randint(1,3)
+	print()
+	
+	crypt = ""
+	aph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	while pos < length:
+		new_position = aph.index(text[pos]) + pasw
+	if new_position >= 26:
+		new_position = new_position - 26
+	text[pos]=aph[new_position]
+	crypt = crypt + text[pos]
+	pos = pos +1
+	
+	user_guess = str(input("Enter your guess\n")) 
+	user_guess = user_guess.replace(" ", "")
+	user_guess = user_guess.lower()
+	while game_win == False:
+		if user_guess == k:
+			game_win == True
+		else:
+			print("Incorect guess, try again")
+	
+
+t1 = Thread(target = exit_timer)
+t2 = Thread(target = caesar_encrypt)
+t1.start()
+t2.start()  
