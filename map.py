cd76f13ef1377096ee0159bcf83c1642c121f8b1
charlywhitlow <charlywhitlow@gmail.com>
2016-10-19 18:02:55 +0100

    update map
    removed exits description from room description

--- map.py
+++ map.py
@@ -7,10 +7,7 @@ room_cell_a = {
     """There are dripping noises echoing throughout the cell and lots 
 of snoring coming from other cells. There's a small sink and toilet 
 in the corner of the room and a little beam of moonlight coming from 
-a small window.
-
-From your knowledge there's an exit south that leads to the Kitchen and 
-a Cleaning Closet to the east. But you also notice a mysterious door to the west.""",
+a small window.""",
 
     "exits": {"west": "Library", "east": "Cleaning Closet", "south": "Kitchen"},
 
@@ -23,10 +20,7 @@ room_cell_b = {
     "description":
     """The cell is almost empty apart from a bunk bed, a sink and a toilet. 
 Everything is bolted in place for safety purposes therefore nothing can 
-be taken from the cell.
-
-You know that to the west there's a Cleaning Closet, 
-to the south is the Showers. But you are unsure of what is to the east.""",
+be taken from the cell.""",
 
     "exits":  {"west": "Cleaning Closet", "east": "Library", "south": "Shower"},
 
@@ -54,10 +48,7 @@ room_kitchen = {
     "description":
     """You are in a large plain room with lots of benches lined up ready 
 to accept prisoners for their meals. There's a cutlery stand and you notice there's
-one knife that hasn't been cleared away.
-
-There's a door leading north back to cell A, an exit east leads to the Showers 
-and finally south there's a large door leading to the Courtyard.""",
+one knife that hasn't been cleared away.""",
 
     "exits": {"south": "Courtyard", "east": "Shower", "north": "Cell A"},
 
@@ -71,10 +62,7 @@ room_shower = {
     """This room is infested with damp and the walls and ceiling appear to 
 be peeling apart. The floor is slippery and you find it hard to walk 
 around the room without falling over. Underneath one of the shower 
-heads is a bar of soap.
-
-From here you can head north towards Cell B, west to the Kitchen or 
-south out to the Courtyard.""",
+heads is a bar of soap.""",
 
     "exits": {"north": "Cell B", "west": "Kitchen", "south": "Courtyard"},
 
@@ -89,9 +77,7 @@ room_courtyard = {
 and lifeless. There are large watchtowers overlooking the courtyard but none 
 of them seem to be manned at the moment. In the centre is some gym equipment 
 and the outside is surrounded by benches. One of the benches seems to have a 
-figure sat on it.
-
-You can go north to the Kitchen, east to the Shower or south to the Warden's Office.""",
+figure sat on it.""",
     
    "exits": {"north": "Kitchen", "east": "Shower", "south": "Warden's Office"},
 
@@ -105,9 +91,7 @@ room_warden = {
     """The Warden's office is very smart with shelves spread around the room holding 
 books and other important looking documents. In the centre on the room there's a 
 large desk with draws overflowing with paperwork. On the south side of the room has 
-a door with the words fire escape written above it.
-
-To the north of the room there's an exit that leads back to the courtyard.""",
+a door with the words fire escape written above it.""",
 
     "exits": {"north": "Courtyard", "south": "Exit"},
 
@@ -135,9 +119,7 @@ room_library = {
     """You enter the mysterious room and realise that this is the prison library.
 It looks barely ever used as all the books are covered in dust and some are still
 in boxes around the room. There's lots of paper around the room and the desks are
-covered in important looking documents.
-
-East heads back to Cell B and the west leads to Cell A.""",
+covered in important looking documents.""",
 
     "exits": {"east": "Cell B", "west": "Cell A" },
 
