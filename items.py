fb8c47a814edeb000279206d71b08f5fce378a4e
charlywhitlow <charlywhitlow@gmail.com>
2016-10-19 13:18:28 +0100

    Charly-most-up-to-date-version
    a few tweaks

---
+++ items.py
@@ -0,0 +1,70 @@
+item_disguise = {
+    "id": "disguise",
+    
+    "name": "a janitor's disguise",
+
+    "damage": 0,
+
+    "trick": 10,
+
+    "description": 
+    """An old Janitor's overalls stained with all kinds of products. 
+It's a bit worn and old but should help hide you from guards."""
+}
+
+item_shank = {
+  "id": "shank",
+
+   "name": "a shank",
+
+   "damage": 10,
+
+    "trick": 0,
+
+  "description": 
+  """It appears to be a sharpened toothbrush. 
+This has been specifically made to harm people."""
+}
+
+
+item_bknife = {
+    "id": "knife",
+
+    "name": "a butter knife",
+
+    "damage": 5,
+
+    "trick": 0,
+
+    "description": 
+    """An old looking kitchen knife that has seen better days. 
+It's so blunt you would be lucky if it cut through butter."""
+}
+
+item_soap = {
+    "id" : "soap",
+
+    "name": "a bar of soap",
+
+    "damage": 0,
+
+    "trick": 0,
+
+    "description": 
+    """An aged bar of soap that is caked in dirt. 
+You think it must've been there for years. 
+‘I probably shouldn't drop this' you mutter to yourself."""
+}
+
+item_key = {
+    "id" : "key",
+
+    "name": "Warden Kirill's key",
+
+    "damage": 0,
+
+    "trick": 0,
+
+    "description" : 
+    """An old looking key. It looks as if it could open something important."""
+}
---
+++ player.py
@@ -0,0 +1,17 @@
+from items import *
+from map import rooms
+from random import randint
+
+inventory = [item_shank, item_disguise]
+
+
+# set start room and current room at start
+f = randint(0,2)
+
+if f ==1:
+    current_room = rooms["Cell A"]
+    start_room = current_room
+else:
+    current_room = rooms["Cell B"]
+    start_room = current_room
+
